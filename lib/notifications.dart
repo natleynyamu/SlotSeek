340506d4ade8e9454a89f06e9881acf195e56a48
NatleyNyamukondiwa <natleynyamukondiwa@gmail.com>
2024-03-01 02:22:39 +0200

    added notification page and edited profile page

--- lib/notifications.dart
+++ lib/notifications.dart
@@ -1,118 +1,250 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:slot_seek/app_colors.dart';
 import 'package:slot_seek/home.dart';
 import 'package:slot_seek/profile.dart';
-
-import 'custom_widgets.dart';
 
 class NotificationsPage extends StatelessWidget {
   const NotificationsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
+        child: AppBar(
+          title: const Text('Notifications'),
+          centerTitle: true,
+          backgroundColor: AppColors.primaryColor,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20),
+            ),
+          ),
+        ),
+      ),
+      body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
             colors: [
               AppColors.primaryGradientStart,
-              AppColors.primaryGradientEnd
+              AppColors.primaryGradientEnd,
             ],
           ),
         ),
-        child: Scaffold(
-          appBar: PreferredSize(
-            preferredSize: const Size.fromHeight(60),
-            child: AppBar(
-              title: const Text('Notifications'),
-              centerTitle: true,
-              backgroundColor: AppColors.primaryColor,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(20),
-                  bottomRight: Radius.circular(20),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                SizedBox(height: 40),
+                Text(
+                  'Today',
+                  style: TextStyle(
+                    color: AppColors.primaryColor,
+                    fontSize: 18,
+                    fontFamily: 'Gabriela',
+                    fontWeight: FontWeight.w400,
+                    height: 0.07,
+                  ),
                 ),
-              ),
+                SizedBox(height: 10),
+                NotificationItem(
+                  image: 'images/success_notification.png',
+                  text:
+                      'Password Reset Successful. Action Password Reset on 11 February at...',
+                ),
+                SizedBox(height: 10),
+                NotificationItem(
+                  image: 'images/time_low.png',
+                  text:
+                      'Time Running Out. Your Booked time is coming to an end. Ext...',
+                ),
+                SizedBox(height: 40),
+                Text(
+                  'Yesterday',
+                  style: TextStyle(
+                    color: AppColors.primaryColor,
+                    fontSize: 18,
+                    fontFamily: 'Gabriela',
+                    fontWeight: FontWeight.w400,
+                    height: 0.07,
+                  ),
+                ),
+                SizedBox(height: 10),
+                NotificationItem(
+                  image: 'images/cancelled_notification.png',
+                  text:
+                      'Booking Cancelled. Action Book a Slot has been Cancelled at...',
+                ),
+                SizedBox(height: 10),
+                NotificationItem(
+                  image: 'images/added_time.png',
+                  text:
+                      'Time Extended Successfully! This is to notify you that time has been ex..',
+                ),
+                SizedBox(height: 40),
+                Text(
+                  'February 28, 2024',
+                  style: TextStyle(
+                    color: AppColors.primaryColor,
+                    fontSize: 18,
+                    fontFamily: 'Gabriela',
+                    fontWeight: FontWeight.w400,
+                    height: 0.07,
+                  ),
+                ),
+                SizedBox(height: 10),
+                NotificationItem(
+                  image: 'images/success_notification.png',
+                  text:
+                      'Payment Successful! Your payment has been successfully proc...',
+                ),
+                SizedBox(height: 10),
+                NotificationItem(
+                 image: 'images/added_time.png',
+                  text:
+                      'Time Extended Successfully! This is to notify you that time has been ex..',
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Text(
+                  'February 27, 2024',
+                  style: TextStyle(
+                    color: AppColors.primaryColor,
+                    fontSize: 18,
+                    fontFamily: 'Gabriela',
+                    fontWeight: FontWeight.w400,
+                    height: 0.07,
+                  ),
+                ),
+                SizedBox(height: 10),
+                NotificationItem(
+                  image: 'images/success_notification.png',
+                  text:
+                      'Payment Successful! Your payment has been successfully proc...',
+                ),
+                SizedBox(height: 10),
+                NotificationItem(
+                  image: 'images/cancelled_notification.png',
+                  text:
+                      'Booking Cancelled. Action Book a Slot has been Cancelled at...',
+                
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Text(
+                  'February 26, 2024',
+                  style: TextStyle(
+                    color: AppColors.primaryColor,
+                    fontSize: 18,
+                    fontFamily: 'Gabriela',
+                    fontWeight: FontWeight.w400,
+                    height: 0.07,
+                  ),
+                ),
+                SizedBox(height: 10),
+                NotificationItem(
+                  image: 'images/success_notification.png',
+                  text:
+                      'Payment Successful! Your payment has been successfully proc...',
+                ),
+                SizedBox(height: 10),
+                NotificationItem(
+                  image: 'images/time_low.png',
+                  text:
+                      'Time Running Out. Your Booked time is coming to an end. Ext...',
+                ),
+              ],
             ),
           ),
-          body: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: AppColors.primaryColor,
+        currentIndex: 1,
+        selectedItemColor: AppColors.secondaryColor,
+        unselectedItemColor: AppColors.greyLight,
+        onTap: (index) {
+          if (index == 0) {
+            // Navigate to Home page
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const HomePage()),
+            );
+          }
+          // Implement navigation logic based on index
+          if (index == 1) {
+            // Navigate to Notification page
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const NotificationsPage()),
+            );
+          }
+          if (index == 2) {
+            // Navigate to profile page
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const ProfilePage()),
+            );
+          }
+        },
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notifications',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
 
+class NotificationItem extends StatelessWidget {
+  final String image;
+  final String text;
 
+  const NotificationItem({
+    Key? key,
+    required this.image,
+    required this.text,
+  }) : super(key: key);
 
-
-                  ])),
-          bottomNavigationBar: ClipRRect(
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(20),
-              topRight: Radius.circular(20),
-            ),
-            child: Container(
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
-                ),
-                color: AppColors.primaryColor,
-              ),
-              child: PreferredSize(
-                preferredSize: const Size.fromHeight(60),
-                child: BottomNavigationBar(
-                  backgroundColor: AppColors.primaryColor,
-                  currentIndex: 1,
-                  selectedItemColor: AppColors.secondaryColor,
-                  unselectedItemColor: AppColors.greyLight,
-                  onTap: (index) {
-                    if (index == 0) {
-                      // Navigate to profile page
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const HomePage()),
-                      );
-                    }
-                    // Implement navigation logic based on index
-                    if (index == 1) {
-                      // Navigate to profile page
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const NotificationsPage()),
-                      );
-                    }
-                    if (index == 2) {
-                      // Navigate to profile page
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const ProfilePage()),
-                      );
-                    }
-                  },
-                  items: const <BottomNavigationBarItem>[
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.home),
-                      label: 'Home',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.notifications),
-                      label: 'Notifications',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.person),
-                      label: 'Profile',
-                    ),
-                  ],
-                ),
-              ),
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: AppColors.greyLight,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        children: [
+          Image.asset(image),
+          const SizedBox(width: 5),
+          Expanded(
+            child: Text(
+              text,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
             ),
           ),
-        ));
+        ],
+      ),
+    );
   }
 }
--- lib/profile.dart
+++ lib/profile.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:slot_seek/app_colors.dart';
-import 'package:slot_seek/edit_profie.dart';
 import 'package:slot_seek/notifications.dart';
 
 import 'home.dart';
@@ -10,270 +9,168 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            AppColors.primaryGradientStart,
-            AppColors.primaryGradientEnd
-          ],
-        ),
-      ),
-      child: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(60),
-          child: AppBar(
-            title: const Text('Profile'),
-            centerTitle: true,
-            backgroundColor: AppColors.primaryColor,
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              ),
-            ),
-          ),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              GestureDetector(
-                onTap: () {},
-                child: SizedBox(
-                  width: 100,
-                  height: 100,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(color: AppColors.greyLight, width: 2),
-                    ),
-                    child: const Icon(
-                      Icons.person,
-                      size: 80,
-                      color: AppColors.greyLight,
+    return Scaffold(
+      backgroundColor: AppColors.greyLight,
+      body: Stack(
+        children: [
+          // Overlapping Container (Blue)
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            height: MediaQuery.of(context).size.height * 0.35,
+            child: Container(
+              color: AppColors.primaryColor, // Blue color
+              child: Center(
+                child: GestureDetector(
+                  onTap: () {},
+                  child: SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border:
+                            Border.all(color: AppColors.greyLight, width: 2),
+                      ),
+                      child: const Icon(
+                        Icons.person,
+                        size: 80,
+                        color: AppColors.greyLight,
+                      ),
                     ),
                   ),
                 ),
               ),
-              const SizedBox(height: 40),
-              Align(
-                alignment: Alignment.topLeft,
+            ),
+          ),
+          // Overlapping Container (Orange)
+          Positioned(
+            bottom: 80, // Adjust the distance from the bottom as needed
+            left: 40, // Adjust the left padding as needed
+            right: 40, // Adjust the right padding as needed
+            height: MediaQuery.of(context).size.height *
+                0.55, // Adjust the height as needed
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                color: AppColors.greyLight,
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Row(
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            // Navigate to Edit Profile screen
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (context) => const EditProfile(),
-                              ),
-                            );
-
-                          },
-                          child: Row(
-                            children: const [
-                              Icon(
-                                Icons.edit,
-                                color: AppColors.primaryLight,
-                                size: 24,
-                              ),
-                              SizedBox(width: 10),
-                              Text(
-                                'Edit Profile',
-                                style: TextStyle(
-                                    color: AppColors.primaryColor,
-                                    fontSize: 20),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+                    const SizedBox(height: 30),
+                    buildRow(context, Icons.edit, 'Edit Profile Name'),
                     const SizedBox(height: 20),
-                    Row(
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            // Handle Help button press
-                          },
-                          child: Row(
-                            children: const [
-                              Icon(
-                                Icons.help,
-                                color: AppColors.primaryLight,
-                                size: 24,
-                              ),
-                              SizedBox(width: 10),
-                              Text(
-                                'Help',
-                                style: TextStyle(
-                                    color: AppColors.primaryColor,
-                                    fontSize: 20),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+                    buildRow(context, Icons.email, 'Change Email Address'),
                     const SizedBox(height: 20),
-                    Row(
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            // Handle Theme button press
-                          },
-                          child: Row(
-                            children: const [
-                              Icon(
-                                Icons.palette,
-                                color: AppColors.textDark,
-                                size: 24,
-                              ),
-                              SizedBox(width: 10),
-                              Text(
-                                'Theme',
-                                style: TextStyle(
-                                    color: AppColors.primaryColor,
-                                    fontSize: 20),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+                    buildRow(context, Icons.lock, 'Change Password'),
                     const SizedBox(height: 20),
-                    Row(
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            // Handle Change Password button press
-                          },
-                          child: Row(
-                            children: const [
-                              Icon(
-                                Icons.lock,
-                                color: AppColors.primaryLight,
-                                size: 24,
-                              ),
-                              SizedBox(width: 10),
-                              Text(
-                                'Change Password',
-                                style: TextStyle(
-                                    color: AppColors.primaryColor,
-                                    fontSize: 20),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+                    buildRow(context, Icons.palette, 'Theme'),
                     const SizedBox(height: 20),
-                    Row(
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            // Handle Logout button press
-                          },
-                          child: Row(
-                            children: const [
-                              Icon(
-                                Icons.exit_to_app,
-                                color: AppColors.secondaryColor,
-                                size: 24,
-                              ),
-                              SizedBox(width: 10),
-                              Text(
-                                'Logout',
-                                style: TextStyle(
-                                    color: AppColors.secondaryColor,
-                                    fontSize: 20),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+                    buildRow(context, Icons.help, 'Help'),
+                    const SizedBox(height: 20),
+                    buildRow(context, Icons.exit_to_app, 'Logout',
+                        isOrange: true),
                   ],
                 ),
               ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+        child: Container(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+            color: AppColors.primaryColor,
+          ),
+          child: PreferredSize(
+            preferredSize: const Size.fromHeight(60),
+            child: BottomNavigationBar(
+              backgroundColor: AppColors.primaryColor,
+              currentIndex: 2,
+              selectedItemColor: AppColors.secondaryColor,
+              unselectedItemColor: AppColors.greyLight,
+              onTap: (index) {
+                if (index == 0) {
+                  // Navigate to Home page
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HomePage()),
+                  );
+                }
+                // Implement navigation logic based on index
+                if (index == 1) {
+                  // Navigate to Notification page
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const NotificationsPage()),
+                  );
+                }
+                if (index == 2) {
+                  // Navigate to profile page
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ProfilePage()),
+                  );
+                }
+              },
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: 'Home',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.notifications),
+                  label: 'Notifications',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person),
+                  label: 'Profile',
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildRow(BuildContext context, IconData icon, String text,
+      {bool isOrange = false}) {
+    return GestureDetector(
+      onTap: () {
+        // Handle button press
+      },
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Icon(
+                icon,
+                color: isOrange ?AppColors.secondaryColor : AppColors.primaryColor,
+                size: 24,
+              ),
+              const SizedBox(width: 10),
+              Text(
+                text,
+                style:  TextStyle(color: isOrange
+                        ? AppColors.secondaryColor
+                        : AppColors.textDark, fontSize: 20),
+              ),
             ],
           ),
-        ),
-        bottomNavigationBar: ClipRRect(
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
-          ),
-          child: Container(
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20),
-                topRight: Radius.circular(20),
-              ),
-              color: AppColors.primaryColor,
-            ),
-            child: PreferredSize(
-              preferredSize: const Size.fromHeight(60),
-              child: BottomNavigationBar(
-                backgroundColor: AppColors.primaryColor,
-                currentIndex: 2,
-                selectedItemColor: AppColors.secondaryColor,
-                unselectedItemColor: AppColors.greyLight,
-                onTap: (index) {
-                  if (index == 0) {
-                    // Navigate to profile page
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const HomePage()),
-                    );
-                  }
-                  if (index == 1) {
-                    // Navigate to profile page
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const NotificationsPage()),
-                    );
-                  }
-                  // Implement navigation logic based on index
-                  if (index == 2) {
-                    // Navigate to profile page
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const ProfilePage()),
-                    );
-                  }
-                },
-                items: const <BottomNavigationBarItem>[
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.home),
-                    label: 'Home',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.notifications),
-                    label: 'Notifications',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.person),
-                    label: 'Profile',
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
+          const SizedBox(height: 5),
+          const Divider(color: AppColors.greyMedium, height: 1), // Divider
+        ],
       ),
     );
   }
